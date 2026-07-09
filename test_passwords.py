34b1fd9980fed245a2e6807975dd4e82b4e78726
LiamNguyenLM <Liam.Nguyen@libertymutual.com>
2026-07-09 13:41:35 -0500

    Delete test_passwords.py

--- test_passwords.py
+++
@@ -1,5 +0,0 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-print("User password set:", bool(os.getenv("USER_PASSWORD")))
-print("Admin password set:", bool(os.getenv("ADMIN_PASSWORD")))
