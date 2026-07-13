87ec9eb3a17fd6833d6a7f676cc2d3b3dc15cbe9
LiamNguyenLM <Liam.Nguyen@libertymutual.com>
2026-07-13 09:46:34 -0500

    Update upload_carrier.py for HO6

--- upload_carrier.py
+++ upload_carrier.py
@@ -13,6 +13,8 @@ def detect_lob_from_name(carrier_name):
         return "HOA"
     if "HOB" in name:
         return "HOB"
+     if "HO6" in name or "HO-6" in name:
+        return "HO6"
     if "HO3" in name or "HO-3" in name or "HOMEOWNERS" in name:
         return "HO3"
     return "Unknown"
